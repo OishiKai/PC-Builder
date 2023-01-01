210bb47fdf93d89083e7370d0efdee43f39c810d
Kai Oishi <bokuchan.kai@gmail.com>
2023-01-01 15:11:02 +0900

    商品画像スライドショーウィジェット追加

---
+++ lib/views/full_scale_image_slider.dart
@@ -0,0 +1,67 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:custom_pc/domain/detail_parser.dart';
+import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+class FullScaleImageSlider extends StatefulWidget {
+  FullScaleImageSlider(this.parser, {Key? key}) : super(key: key);
+  DetailParser parser;
+  @override
+  State<FullScaleImageSlider> createState() => _FullScaleImageSliderState();
+}
+
+class _FullScaleImageSliderState extends State<FullScaleImageSlider> {
+  int activeIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        CarouselSlider.builder(
+          options: CarouselOptions(
+            height: 320,
+            initialPage: 0,
+            viewportFraction: 1,
+            enlargeCenterPage: true,
+            onPageChanged: (index, reason) => setState(() {
+              activeIndex = index;
+            }),
+          ),
+          itemCount: widget.parser.fullScaleImages.length,
+          itemBuilder: (context, index, realIndex) {
+            final path = widget.parser.fullScaleImages[index];
+            return buildImage(path, index);
+          },
+        ),
+        Align(
+          alignment: Alignment.center,
+          child: Column(
+            children: [
+              SizedBox(height: 300,),
+              buildIndicator(),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget buildImage(path, index) => Container(
+    //画像間の隙間
+    margin: EdgeInsets.symmetric(horizontal: 10),
+    color: Colors.white,
+    child: Image.network(
+      path,
+      fit: BoxFit.contain,
+    ),
+  );
+
+  Widget buildIndicator() => AnimatedSmoothIndicator(
+    activeIndex: activeIndex,
+    count: widget.parser.fullScaleImages.length ?? 0,
+    effect: const JumpingDotEffect(
+        dotHeight: 10,
+        dotWidth: 10,
+        activeDotColor: Colors.grey,
+        dotColor: Colors.black12),
+  );
+}
