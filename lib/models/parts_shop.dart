359840cae08224ecef1a86ddb8826bda02dc03c0
Kai Oishi <bokuchan.kai@gmail.com>
2023-02-21 02:11:58 +0900

    販売店舗情報、スペック情報の取得処理

--- lib/models/parts_shop.dart
+++ lib/models/parts_shop.dart
@@ -1,8 +1,9 @@
-// class PartsShop {
-//   final String rank;
-//   final String price;
-//   final String diff;
-//   final
-//   final String name;
-//   final
-// }
+class PartsShop {
+  final String rank;
+  final String price;
+  final String bestPriceDiff;
+  final String shopName;
+  final String shopPageUrl;
+
+  PartsShop(this.rank, this.price, this.bestPriceDiff, this.shopName, this.shopPageUrl);
+}
