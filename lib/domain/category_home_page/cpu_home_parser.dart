94bd7bd81b40d02b6a315416539bfa9aebb54ad0
Kai Oishi <bokuchan.kai@gmail.com>
2023-04-26 22:08:46 +0900

    CPUの検索ホーム画面用の情報パースクラス作成

---
+++ lib/domain/category_home_page/cpu_home_parser.dart
@@ -0,0 +1,104 @@
+import 'package:custom_pc/models/category_home_data.dart';
+import 'package:html/dom.dart';
+
+import '../../models/pc_parts.dart';
+import '../document_repository.dart';
+
+class CpuHomeParser {
+  // パース対象のURL
+  static const _pageUrl = 'https://kakaku.com/pc/cpu/';
+
+  // 世代のシリーズ名
+  static const _cpuSeriesSelector = '#menu > div:nth-child(22) > div > div:nth-child(5) > ul > li';
+
+  // 売れ筋ランキングのリスト
+  static const _popularPartsListSelector = '#ct087 > div.contMain > div.contMainIn > ol > li';
+
+  // 売れ筋ランキングのパーツの画像
+  static const _partsImageSelector = 'div > div.itemImg > img';
+
+  // 売れ筋ランキングのメーカー名
+  static const _makerSelector = 'div > div.itemIn2 > p.makerName';
+
+  // 売れ筋ランキングの商品名
+  static const _titleSelector = 'div > div.itemIn2 > p:nth-child(3)';
+
+  // 売れ筋ランキングの価格
+  static const _priceSelector = 'div > div.itemIn3 > p.itemPrice';
+
+  // 売れ筋ランキングの星の数 "5.00" のような形式
+  static const _starSelector = 'div > div.itemIn3 > p.starRate > span.num1';
+
+  // 売れ筋ランキングの評価数 "（2人）"のような形式
+  static const _evaluationSelector = 'div > div.itemIn3 > p.starRate > span.num2';
+
+  // 詳細画面へのURL
+  static const _detailUrlSelector = 'div > div.itemIn2 > p:nth-child(3) > a';
+
+  // 検索ホーム画面での人気製品の表示数　偶数にすべし
+  static const _popularPartsRequired = 8;
+
+  static Document? _document;
+  static Future<CpuHome?> fetchAndParse() async {
+    _document = await DocumentRepository.fetchDocument(_pageUrl);
+    final chips = _parseCpuSeries()!;
+    final parts = _parsePopularPats()!;
+
+    // 同メソッド内で _document に代入している為、ここのnullは握りつぶす
+    return CpuHome(chips['intel']!, chips['amd']!, parts);
+  }
+
+  static Map<String, List<String>>? _parseCpuSeries() {
+    if (_document == null) {
+      return null;
+    }
+    final cpuSeriesElement = _document!.querySelectorAll(_cpuSeriesSelector);
+    List<String> intelSeries = [];
+    List<String> amdSeries = [];
+    for (var element in cpuSeriesElement) {
+      if (element.text.contains('Core i')) {
+        intelSeries.add(element.text.split('(')[0]);
+      }
+      if (element.text.contains('Ryzen')) {
+        amdSeries.add(element.text.split('(')[0]);
+      }
+    }
+    return {'intel': intelSeries, 'amd': amdSeries};
+  }
+
+  static List<PcParts>? _parsePopularPats() {
+    if (_document == null) {
+      return null;
+    }
+    List<PcParts> partsList = [];
+    final partsListElement = _document!.querySelectorAll(_popularPartsListSelector);
+
+    for (int i = 0; i < _popularPartsRequired; i++) {
+      final element = partsListElement[i];
+      final imageUrl = element.querySelectorAll(_partsImageSelector)[0].attributes['data-src'];
+      final maker = element.querySelectorAll(_makerSelector)[0].text;
+      final title = element.querySelectorAll(_titleSelector)[0].text;
+      final price = element.querySelectorAll(_priceSelector)[0].text;
+
+      // 取得時の形式は "4.95" のようなかたち　PcPartsオブジェクトに渡す際は　"49"のようなかたちにする
+      final tempStar = element.querySelectorAll(_starSelector)[0].text;
+
+      // 取得時の形式は "（2人）"のようなかたち　PcPartsオブジェクトには "4.95(5)" のようなかたちで渡す為、tempStarと組み合わせる
+      final evaluation = '$tempStar${element.querySelectorAll(_evaluationSelector)[0].text.replaceFirst('人', '').replaceFirst('（', '(').replaceFirst('）', ')')}';
+
+      // 取得時、末尾に "?lid=20190108pricemenu_ranking_1" のようなパラメータがつくので除き、PartsListPageでの取得時の形式に整形する
+      final detailUrl = '${element.querySelectorAll(_detailUrlSelector)[0].attributes['href']!.split('?lid')[0]}?lid=pc_ksearch_kakakuitem';
+
+      int? star;
+      if (tempStar != '—') {
+        // "4.95" -> "49" に変換
+        if (double.tryParse(tempStar) != null) {
+          final doubleStar = double.parse(tempStar);
+          star = doubleStar * 100 ~/ 10;
+        }
+      }
+      partsList.add(PcParts(maker, false, title, star, evaluation, price, '$i', imageUrl!, detailUrl!));
+    }
+    return partsList;
+  }
+}
