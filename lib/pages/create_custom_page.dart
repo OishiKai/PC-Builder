436f725ebabf318156aaf6c75c84e92db2c1cb73
Kai Oishi <bokuchan.kai@gmail.com>
2023-06-04 18:02:09 +0900

    互換性Widgetをスクロールさせず、画面全体をスクロールするようにした

--- lib/pages/create_custom_page.dart
+++ lib/pages/create_custom_page.dart
@@ -16,80 +16,102 @@ class CreateCustomPage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     SizeConfig().init(context);
     return Scaffold(
-      backgroundColor: const Color(0xFFEDECF2),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      backgroundColor: _mainColor,
+      //backgroundColor: const Color(0xFFEDECF2),
+      body: ListView(
+        //crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Padding(
-            padding: const EdgeInsets.only(top: 40, left: 16, right: 16),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                InkWell(
-                  onTap: () {
-                    ref.read(customProvider.notifier).reset();
-                    Navigator.pop(context);
-                  },
-                  child: Icon(
-                    Icons.arrow_back,
-                    size: 40,
-                    color: _mainColor,
-                  ),
-                ),
-                const Spacer(
-                  flex: 2,
-                ),
-                Text(
-                  'NEW CUSTOM',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: _mainColor,
-                  ),
-                ),
-                const Spacer(
-                  flex: 3,
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          const PartsScrollWidget(),
-          const SizedBox(
-            height: 16,
-          ),
-
-          // 合計金額・割合ウィジェット
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: SizeConfig.blockSizeHorizontal * 4),
-            child: TotalPriceWidget(),
-          ),
-
-          const SizedBox(
-            height: 16,
-          ),
-
-          if (ref.watch(customProvider).compatibilities != null)
-            NotificationListener(
-              onNotification: (OverscrollIndicatorNotification notification) {
-                notification.disallowGlow();
-                return false;
-              },
+            padding: const EdgeInsets.only(top: 8),
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: SizeConfig.blockSizeVertical * 90,
+              ),
               child: Container(
-                height: 290,
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.vertical,
-                  primary: false,
-                  child: Column(
-                    children: [
+                //height: SizeConfig.blockSizeVertical * 90,
+                decoration: BoxDecoration(
+                  color: const Color(0xFFEDECF2),
+                  borderRadius: BorderRadius.circular(40),
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 12, left: 16, right: 16),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          InkWell(
+                            onTap: () {
+                              ref.read(customProvider.notifier).reset();
+                              Navigator.pop(context);
+                            },
+                            child: Icon(
+                              Icons.arrow_back,
+                              size: 40,
+                              color: _mainColor,
+                            ),
+                          ),
+                          const Spacer(
+                            flex: 2,
+                          ),
+                          Text(
+                            'NEW CUSTOM',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: _mainColor,
+                            ),
+                          ),
+                          const Spacer(
+                            flex: 3,
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    const PartsScrollWidget(),
+                    const SizedBox(
+                      height: 16,
+                    ),
+
+                    // 合計金額・割合ウィジェット
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: SizeConfig.blockSizeHorizontal * 4),
+                      child: TotalPriceWidget(),
+                    ),
+
+                    const SizedBox(
+                      height: 16,
+                    ),
+
+                    // if (ref.watch(customProvider).compatibilities != null)
+                    //   NotificationListener(
+                    //     onNotification: (OverscrollIndicatorNotification notification) {
+                    //       notification.disallowGlow();
+                    //       return false;
+                    //     },
+                    //     child: Container(
+                    //       height: 290,
+                    //       child: SingleChildScrollView(
+                    //         scrollDirection: Axis.vertical,
+                    //         primary: false,
+                    //         child: Column(
+                    //           children: [
+                    //             for (int i = 0; i < ref.watch(customProvider).compatibilities!.length; i++) PartsCompatibilityWidget(ref.watch(customProvider).compatibilities![i]),
+                    //           ],
+                    //         ),
+                    //       ),
+                    //     ),
+                    //   )
+                    if (ref.watch(customProvider).compatibilities != null)
                       for (int i = 0; i < ref.watch(customProvider).compatibilities!.length; i++) PartsCompatibilityWidget(ref.watch(customProvider).compatibilities![i]),
-                    ],
-                  ),
+                  ],
                 ),
               ),
-            )
+            ),
+          ),
         ],
       ),
     );
