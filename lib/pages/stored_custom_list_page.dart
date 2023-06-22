d8a197b9795fcd0c8d21ebbf1894a34aabb7b7fd
Kai Oishi <bokuchan.kai@gmail.com>
2023-06-22 22:12:52 +0900

    保存済みカスタムタイル追加

--- lib/pages/stored_custom_list_page.dart
+++ lib/pages/stored_custom_list_page.dart
@@ -1,4 +1,5 @@
 import 'package:custom_pc/config/size_config.dart';
+import 'package:custom_pc/widgets/stored_custom_list/custom_tile.dart';
 import 'package:flutter/material.dart';
 
 class StoredCustomListPage extends StatelessWidget {
@@ -31,9 +32,9 @@ class StoredCustomListPage extends StatelessWidget {
                 ),
                 const Spacer(),
                 const Text(
-                  '見積もりリスト',
+                  'ライブラリ',
                   style: TextStyle(
-                    fontSize: 20,
+                    fontSize: 18,
                     color: mainColor,
                     fontWeight: FontWeight.bold,
                   ),
@@ -54,138 +55,21 @@ class StoredCustomListPage extends StatelessWidget {
           ),
           const SizedBox(height: 8),
           Container(
-              height: SizeConfig.blockSizeVertical * 80,
-              padding: const EdgeInsets.only(top: 30),
-              decoration: BoxDecoration(
-                color: const Color(0xFFEDECF2),
-                borderRadius: BorderRadius.circular(40),
-              ),
-              child: Column(
-                children: [
-                  Container(
-                    decoration: const BoxDecoration(
-                      border: Border(
-                        bottom: BorderSide(
-                          color: Colors.white,
-                          width: 4,
-                        ),
-                      ),
-                    ),
-                    child: Theme(
-                      data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
-                      child: ExpansionTile(
-                        backgroundColor: const Color(0xFFEDECF2),
-                        collapsedIconColor: mainColor,
-                        iconColor: mainColor,
-                        //collapsedBackgroundColor: Colors.redAccent,
-                        collapsedTextColor: Colors.blue,
-                        title: Row(
-                          children: [
-                            const Icon(
-                              Icons.description_outlined,
-                              size: 30,
-                              color: mainColor,
-                            ),
-                            const SizedBox(width: 10),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: const [
-                                Text(
-                                  'あああああああああああああああ',
-                                  maxLines: 1,
-                                  textAlign: TextAlign.right,
-                                  overflow: TextOverflow.ellipsis,
-                                  style: TextStyle(
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.bold,
-                                    color: mainColor,
-                                  ),
-                                ),
-                                Text(
-                                  '2021/09/01',
-                                  style: TextStyle(
-                                    fontSize: 10,
-                                    fontWeight: FontWeight.bold,
-                                    color: mainColor,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                        children: const [
-                          ListTile(
-                            title: Text(
-                              'CPU',
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Container(
-                    decoration: const BoxDecoration(
-                      border: Border(
-                        bottom: BorderSide(
-                          color: Colors.white,
-                          width: 4,
-                        ),
-                      ),
-                    ),
-                    child: Theme(
-                      data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
-                      child: ExpansionTile(
-                        backgroundColor: const Color(0xFFEDECF2),
-                        //collapsedIconColor: mainColor,
-                        //collapsedBackgroundColor: Colors.redAccent,
-                        collapsedTextColor: Colors.blue,
-                        title: Row(
-                          children: const [
-                            Icon(
-                              Icons.storage_sharp,
-                              size: 30,
-                              color: mainColor,
-                            ),
-                            SizedBox(width: 8),
-                            Text(
-                              'カスタム名',
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                                color: mainColor,
-                              ),
-                            ),
-                            Spacer(),
-                            Text(
-                              '¥ 100,000',
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                                color: mainColor,
-                              ),
-                            ),
-                          ],
-                        ),
-                        children: const [
-                          ListTile(
-                            title: Text(
-                              'CPU',
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              )),
+            padding: const EdgeInsets.only(top: 16),
+            height: SizeConfig.blockSizeVertical * 80,
+            decoration: BoxDecoration(
+              color: const Color(0xFFEDECF2),
+              borderRadius: BorderRadius.circular(40),
+            ),
+            child: ListView.builder(
+              padding: EdgeInsets.zero,
+              shrinkWrap: true,
+              itemCount: 4,
+              itemBuilder: (BuildContext context, int index) {
+                return const CustomTile();
+              },
+            ),
+          ),
         ],
       ),
     ));
