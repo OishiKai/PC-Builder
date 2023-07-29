3e96073c15c0c53a41e954773f56e9bc2c3569a2
Kai Oishi <bokuchan.kai@gmail.com>
2023-07-29 11:35:05 +0900

    価格順、作成日順でカスタムをソートできるようにした

---
+++ lib/widgets/stored_custom_list/sort_icon_button.dart
@@ -0,0 +1,39 @@
+import 'package:custom_pc/providers/stored_customs.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+enum SortState {
+  date,
+  price,
+}
+
+class SortIconButton extends ConsumerWidget {
+  SortIconButton({super.key});
+  final alignState = StateProvider<SortState>((ref) => SortState.date);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    const mainColor = Color.fromRGBO(60, 130, 80, 1);
+    final sort = ref.watch(alignState);
+    return IconButton(
+      onPressed: () {
+        if (sort == SortState.date) {
+          ref.read(storedCustomsNotifierProvider.notifier).sortCustomsByPrice();
+          ref.read(alignState.notifier).update((state) => SortState.price);
+        } else {
+          ref.read(storedCustomsNotifierProvider.notifier).sortCustomsByCreateDate();
+          ref.read(alignState.notifier).update((state) => SortState.date);
+        }
+      },
+      icon: sort == SortState.date
+          ? const Icon(
+              Icons.calendar_month_sharp,
+              color: mainColor,
+            )
+          : const Icon(
+              Icons.sort_sharp,
+              color: mainColor,
+            ),
+    );
+  }
+}
