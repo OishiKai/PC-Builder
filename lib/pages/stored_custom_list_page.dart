db68c6e51e00c05f3a31d97ca770f508f5eaba0c
Kai Oishi <bokuchan.kai@gmail.com>
2023-07-29 13:45:50 +0900

    初回起動時にチュートリアル画面表示

--- lib/pages/stored_custom_list_page.dart
+++ lib/pages/stored_custom_list_page.dart
@@ -4,12 +4,29 @@ import 'package:custom_pc/widgets/stored_custom_list/new_custom_bottom_bar.dart'
 import 'package:custom_pc/widgets/stored_custom_list/sort_icon_button.dart';
 import 'package:custom_pc/widgets/stored_custom_list/stored_customs_table_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class StoredCustomListPage extends StatelessWidget {
   const StoredCustomListPage({super.key});
 
+  void _showTutorial(BuildContext context) async {
+    final pref = await SharedPreferences.getInstance();
+
+    if (pref.getBool('isAlreadyFirstLaunch') != true) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => FlutterOverboardPage(),
+          fullscreenDialog: true,
+        ),
+      );
+      pref.setBool('isAlreadyFirstLaunch', true);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback((_) => _showTutorial(context));
     const mainColor = Color.fromRGBO(60, 130, 80, 1);
     SizeConfig().init(context);
     return Scaffold(
