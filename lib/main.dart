50794d669e068f60b43d5f5b6f11e83801fd7bb2
Kai Oishi <bokuchan.kai@gmail.com>
2023-07-30 22:27:24 +0900

    不要コード削除

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'package:custom_pc/domain/database/custom_repository.dart';
-import 'package:custom_pc/pages/create_custom_page.dart';
 import 'package:custom_pc/pages/stored_custom_list_page.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -29,67 +26,6 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: StoredCustomListPage(),
-    );
-  }
-}
-
-class RootPage extends ConsumerWidget {
-  const RootPage({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    PageRouteBuilder(
-                        pageBuilder: (context, animation, secondaryAnimation) => const CreateCustomPage(),
-                        transitionsBuilder: (context, animation, secondaryAnimation, child) {
-                          return CupertinoPageTransition(primaryRouteAnimation: animation, secondaryRouteAnimation: secondaryAnimation, linearTransition: false, child: child);
-                        }),
-                  );
-                },
-                child: const Text('見積もりを作成する')),
-            ElevatedButton(
-                onPressed: () async {
-                  final storedCustoms = await CustomRepository.getAllCustoms();
-                  storedCustoms.forEach((key, value) {
-                    print('$key: ${value.name}');
-                  });
-                  Navigator.push(
-                    context,
-                    PageRouteBuilder(
-                        pageBuilder: (context, animation, secondaryAnimation) => StoredCustomListPage(),
-                        transitionsBuilder: (context, animation, secondaryAnimation, child) {
-                          return CupertinoPageTransition(primaryRouteAnimation: animation, secondaryRouteAnimation: secondaryAnimation, linearTransition: false, child: child);
-                        }),
-                  );
-                },
-                child: const Text('保存済みカスタム')),
-            // ElevatedButton(
-            //     onPressed: () async {
-            //       final storedCustoms = await CustomRepository.getAllCustoms();
-            //       storedCustoms?.forEach((key, value) {
-            //         print('$key: ${value.name}');
-            //       });
-            //       Navigator.push(
-            //         context,
-            //         PageRouteBuilder(
-            //             pageBuilder: (context, animation, secondaryAnimation) => MyWidget(),
-            //             transitionsBuilder: (context, animation, secondaryAnimation, child) {
-            //               return CupertinoPageTransition(primaryRouteAnimation: animation, secondaryRouteAnimation: secondaryAnimation, linearTransition: false, child: child);
-            //             }),
-            //       );
-            //     },
-            //     child: const Text('隠れる')),
-          ],
-        ),
-      ),
     );
   }
 }
