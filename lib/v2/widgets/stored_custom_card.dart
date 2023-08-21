4317acd7fd12a6966528f5b67a421513f661eb43
Kai Oishi <bokuchan.kai@gmail.com>
2023-08-21 18:14:50 +0900

    保存済みカスタムリスト追加

---
+++ lib/v2/widgets/stored_custom_card.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+import '../../models/custom.dart';
+
+class StoredCustomCard extends StatelessWidget {
+  const StoredCustomCard({super.key, required this.custom});
+  final Custom custom;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Theme.of(context).colorScheme.secondaryContainer,
+      elevation: 4,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Column(children: [
+          Row(
+            children: [
+              SizedBox(
+                  height: 60.0,
+                  width: 60.0,
+                  child: ClipRRect(
+                      borderRadius: BorderRadius.circular(100),
+                      child: Image.network(
+                        custom.getRandomPartsImage(),
+                        fit: BoxFit.cover,
+                        colorBlendMode: BlendMode.darken,
+                      ))),
+              const SizedBox(width: 16),
+              Text(
+                custom.name!,
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              ),
+              const Spacer(),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    custom.formatPrice(),
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.secondary,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    custom.date!,
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.secondary,
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          )
+        ]),
+      ),
+    );
+  }
+}
--- lib/v2/widgets/stored_custom_list_widget.dart
+++ lib/v2/widgets/stored_custom_list_widget.dart
@@ -1,13 +1,57 @@
+import 'package:custom_pc/providers/stored_customs.dart';
+import 'package:custom_pc/v2/widgets/stored_custom_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../models/custom.dart';
 
 class StoredCustomListWidget extends ConsumerWidget {
   const StoredCustomListWidget({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Center(
-      child: Text('Stored Custom List'),
-    );
+    final storedCustoms = ref.watch(storedCustomsNotifierProvider);
+
+    return storedCustoms.when(data: (data) {
+      final customList = [];
+      data.forEach((key, value) => customList.add(_StoredCustom(id: key, custom: value)));
+
+      return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: NotificationListener<OverscrollIndicatorNotification>(
+          onNotification: (OverscrollIndicatorNotification notification) {
+            notification.disallowIndicator();
+            return false;
+          },
+          child: ListView.builder(
+            itemCount: data.length,
+            itemBuilder: (context, index) {
+              return StoredCustomCard(
+                custom: customList[index].custom,
+              );
+            },
+          ),
+        ),
+      );
+    }, loading: () {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }, error: (e, s) {
+      return const Center(
+        child: Text('読み込みに失敗しました'),
+      );
+    });
   }
+}
+
+// MapをListにする用のクラス
+class _StoredCustom {
+  _StoredCustom({
+    required this.id,
+    required this.custom,
+  });
+
+  final String id;
+  final Custom custom;
 }
