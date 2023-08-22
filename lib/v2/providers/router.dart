12fb57ad53a00445544d91be92b34b035202a9ec
Kai Oishi <bokuchan.kai@gmail.com>
2023-08-22 22:24:48 +0900

    BottomNavigationBarでの画面遷移実装

--- lib/v2/providers/router.dart
+++ lib/v2/providers/router.dart
@@ -1,14 +1,99 @@
+import 'package:custom_pc/v2/dashboard.dart';
+import 'package:custom_pc/v2/next_tab_page.dart';
 import 'package:custom_pc/v2/stored_custom_list_page.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
+import '../next_page.dart';
+import '../sub_route_page.dart';
+
+// このGlobalKeyは、GoRouterのインスタンスを取得するために必要です。
+final _rootNavigatorKey = GlobalKey<NavigatorState>();
+// このGlobalKeyは、各タブのGoRouterインスタンスを取得するために必要です。
+final _sectionNavigatorKey = GlobalKey<NavigatorState>();
+
 final routerProvider = Provider<GoRouter>((ref) {
   return GoRouter(
+    initialLocation: '/home',
     routes: [
+      // GoRoute(
+      //   path: '/',
+      //   builder: (context, state) => const StoredCustomListPageV2(),
+      // ),
       GoRoute(
-        path: '/',
-        builder: (context, state) => const StoredCustomListPageV2(),
-      )
+        path: '/next',
+        pageBuilder: (context, state) => const MaterialPage(
+          fullscreenDialog: true,
+          child: NextPage(),
+        ),
+      ),
+      StatefulShellRoute.indexedStack(
+        builder: (context, state, navigationShell) {
+          // カスタムシェル（BottomNavigationBar など）を実装したウィジェットを返します。
+          // ステートフルな方法で他のブランチにナビゲートできるように、 // [StatefulNavigationShell] が渡されます。
+          return Dashboard(navigationShell: navigationShell);
+        },
+        // branchesとは、IndexedStackの子ウィジェットとして表示されるGoRouterのブランチを指定します。
+        branches: [
+          // ボトムナビゲーションバーのルート分岐1
+          // StatefulShellBranch(
+          //   navigatorKey: _sectionNavigatorKey,
+          //   // このブランチルートを追加する
+          //   // 各ルートとそのサブルート (利用可能な場合) 例: feed/uuid/details
+          //   routes: <RouteBase>[
+          //     GoRoute(
+          //       path: '/',
+          //       builder: (context, state) => const StoredCustomListPageV2(),
+          //       routes: <RouteBase>[
+          //         GoRoute(
+          //           path: RouterPath.details,
+          //           builder: (context, state) {
+          //             return const DetailsPage(label: 'FeedDetailsをFullPathで渡す');
+          //           },
+          //         )
+          //       ],
+          //     ),
+          //   ],
+          // ),
+          // ボトムナビゲーションバーのルート分岐2
+          StatefulShellBranch(
+            // navigatorKey: _sectionNavigatorKey,
+            // initialLocation: '/',
+            routes: [
+              // このブランチルートを追加する
+              // 各ルートとそのサブルート (利用可能な場合) 例: shope/uuid/details
+              GoRoute(
+                path: '/home',
+                builder: (context, state) {
+                  return const StoredCustomListPageV2();
+                },
+                routes: [
+                  GoRoute(
+                    path: 'sub_route',
+                    builder: (context, state) {
+                      return const SubRoutePage();
+                    },
+                  )
+                ],
+              ),
+            ],
+          ),
+          // ボトムナビゲーションバーのルート分岐3を追加
+          StatefulShellBranch(
+            routes: [
+              // このブランチルートを追加する
+              // 各ルートとそのサブルート (利用可能な場合) 例: profile/uuid/details
+              GoRoute(
+                path: '/nextTab',
+                builder: (context, state) {
+                  return const NextTabPage();
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
     ],
   );
 });
--- lib/v2/stored_custom_list_page.dart
+++ lib/v2/stored_custom_list_page.dart
@@ -1,6 +1,9 @@
 import 'package:custom_pc/v2/widgets/sort_icon_button.dart';
 import 'package:custom_pc/v2/widgets/stored_custom_list_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../widgets/edit_custom/add_parts_modal_widget.dart';
 
 class StoredCustomListPageV2 extends StatelessWidget {
   const StoredCustomListPageV2({super.key});
@@ -8,79 +11,64 @@ class StoredCustomListPageV2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.background,
-          elevation: 0,
-          centerTitle: true,
-          title: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                'ライブラリ',
-                style: TextStyle(
-                  fontSize: 24,
-                  color: Theme.of(context).colorScheme.primary,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                '保存済みカスタム',
-                style: TextStyle(
-                  fontSize: 14,
-                  color: Theme.of(context).colorScheme.secondary,
-                  fontWeight: FontWeight.bold,
-                ),
-              )
-            ],
-          ),
-          leading: SortIconButton(),
-          actions: [
-            IconButton(
-              onPressed: () {
-                debugPrint('on pressed question mark icon button');
-              },
-              icon: Icon(
-                Icons.help_outline,
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        elevation: 0,
+        centerTitle: true,
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'ライブラリ',
+              style: TextStyle(
+                fontSize: 24,
                 color: Theme.of(context).colorScheme.primary,
-                size: 32,
+                fontWeight: FontWeight.bold,
               ),
             ),
+            Text(
+              '保存済みカスタム',
+              style: TextStyle(
+                fontSize: 14,
+                color: Theme.of(context).colorScheme.secondary,
+                fontWeight: FontWeight.bold,
+              ),
+            )
           ],
         ),
-        body: const StoredCustomListWidget(),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            debugPrint('on pressed add icon button');
-          },
-          backgroundColor: Theme.of(context).colorScheme.primary,
-          foregroundColor: Theme.of(context).colorScheme.onPrimary,
-          child: const Icon(
-            Icons.add,
-            size: 32,
+        leading: SortIconButton(),
+        actions: [
+          IconButton(
+            onPressed: () {
+              context.push('/sub_route');
+            },
+            icon: Icon(
+              Icons.help_outline,
+              color: Theme.of(context).colorScheme.primary,
+              size: 32,
+            ),
           ),
+        ],
+      ),
+      body: const StoredCustomListWidget(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+            isScrollControlled: true,
+            backgroundColor: Colors.transparent,
+            context: context,
+            builder: (BuildContext context) {
+              return AddPartsModalWidget(() {});
+            },
+          );
+        },
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        foregroundColor: Theme.of(context).colorScheme.onPrimary,
+        child: const Icon(
+          Icons.add,
+          size: 32,
         ),
-        bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: Theme.of(context).colorScheme.background,
-          selectedItemColor: Theme.of(context).colorScheme.primary,
-          unselectedItemColor: Theme.of(context).colorScheme.secondary,
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'ホーム',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.search),
-              label: '検索',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.book),
-              label: 'ライブラリ',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.settings),
-              label: '設定',
-            ),
-          ],
-        ));
+      ),
+    );
   }
 }
