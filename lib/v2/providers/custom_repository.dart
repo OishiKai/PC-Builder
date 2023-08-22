c8cd536f02358b766ea22b0c21e6d0174a41523d
Kai Oishi <bokuchan.kai@gmail.com>
2023-08-22 23:35:33 +0900

    保存済みカスタム用のプロバイダー追加

---
+++ lib/v2/providers/custom_repository.dart
@@ -0,0 +1,55 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../../domain/database/custom_repository.dart';
+import '../../models/custom.dart';
+import '../../widgets/stored_custom_list/sort_icon_button.dart';
+
+part 'gen/custom_repository.g.dart';
+
+@riverpod
+class CustomRepositoryNotifier extends _$CustomRepositoryNotifier {
+  @override
+  Future<List<Custom>> build() {
+    return CustomRepository.getAllCustomsV2();
+  }
+
+  void refresh() async {
+    ref.read(alignState.notifier).update((state) => SortState.date);
+    state = await AsyncValue.guard(() async {
+      return CustomRepository.getAllCustomsV2();
+    });
+  }
+
+  void addCustom(Custom custom) async {
+    await CustomRepository.insertCustom(custom);
+    refresh();
+  }
+
+  void deleteCustom(String id) async {
+    await CustomRepository.deleteCustom(id);
+    refresh();
+  }
+
+  void updateCustom(Custom custom, String id) async {
+    await CustomRepository.updateCustom(id, custom);
+    refresh();
+  }
+
+  void sortCustomsByPrice() async {
+    state.when(
+      data: (data) {
+        // 価格降順でソート
+        data.sort((a, b) => b.calculateTotalPrice().compareTo(a.calculateTotalPrice()));
+        state = AsyncValue.data(data);
+      },
+      error: (e, t) {},
+      loading: () {},
+    );
+  }
+
+  void sortCustomsByCreateDate() async {
+    state = await AsyncValue.guard(() async {
+      return CustomRepository.getAllCustomsV2();
+    });
+  }
+}
