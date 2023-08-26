ec5df1be2514f02ae980a721e1c0d1a949164153
Kai Oishi <bokuchan.kai@gmail.com>
2023-08-27 01:38:11 +0900

    カスタムの作成・編集用のprovider追加

---
+++ lib/v2/providers/edit_custom.dart
@@ -0,0 +1,87 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../../domain/compatibility_analyzer.dart';
+import '../../models/custom.dart';
+import '../../models/parts_compatibility.dart';
+import '../../models/pc_parts.dart';
+
+part 'edit_custom.g.dart';
+
+@Riverpod(keepAlive: true)
+class EditCustomNotifier extends _$EditCustomNotifier {
+  @override
+  Custom build() {
+    return const Custom();
+  }
+
+  /// 保存済みのカスタムを編集する場合
+  void setCustom(Custom custom) {
+    state = custom;
+    updateCompatibilities();
+  }
+
+  /// カスタム名を変更する
+  void rename(String name) {
+    state = state.copyWith(name: name);
+  }
+
+  /// パーツを追加・変更
+  void setParts(PartsCategory category, PcParts parts) {
+    switch (category) {
+      case PartsCategory.cpu:
+        state = state.copyWith(cpu: parts);
+        break;
+      case PartsCategory.cpuCooler:
+        state = state.copyWith(cpuCooler: parts);
+        break;
+      case PartsCategory.memory:
+        state = state.copyWith(memory: parts);
+        break;
+      case PartsCategory.motherboard:
+        state = state.copyWith(motherBoard: parts);
+        break;
+      case PartsCategory.graphicsCard:
+        state = state.copyWith(graphicsCard: parts);
+        break;
+      case PartsCategory.ssd:
+        state = state.copyWith(ssd: parts);
+        break;
+      case PartsCategory.powerUnit:
+        state = state.copyWith(powerUnit: parts);
+        break;
+      case PartsCategory.pcCase:
+        state = state.copyWith(pcCase: parts);
+        break;
+      case PartsCategory.caseFan:
+        state = state.copyWith(caseFan: parts);
+        break;
+    }
+  }
+
+  /// 互換性情報を更新
+  void updateCompatibilities() {
+    List<PartsCompatibility> comps = [];
+    // 互換性チェック
+    if (state.cpu != null && state.motherBoard != null) {
+      final compatibility = CompatibilityAnalyzer.analyzeCpuAndMotherBoard(cpu: state.cpu!, motherBoard: state.motherBoard!);
+      comps.add(compatibility);
+    }
+
+    if (state.cpuCooler != null && state.motherBoard != null) {
+      final compatibility = CompatibilityAnalyzer.analyzeCpuCoolerAndMotherBoard(cpuCooler: state.cpuCooler!, motherBoard: state.motherBoard!);
+      comps.add(compatibility);
+    }
+
+    if (state.memory != null && state.motherBoard != null) {
+      final compatibility = CompatibilityAnalyzer.analyzeMemoryAndMotherBoard(memory: state.memory!, motherBoard: state.motherBoard!);
+      comps.add(compatibility);
+    }
+
+    if (state.motherBoard != null && state.ssd != null) {
+      final compatibility = CompatibilityAnalyzer.analyzeMotherBoardAndSsd(motherBoard: state.motherBoard!, ssd: state.ssd!);
+      comps.add(compatibility);
+    }
+    final cus = state.copyWith(compatibilities: comps);
+    state = cus;
+  }
+}
---
+++ lib/v2/providers/edit_custom.g.dart
@@ -0,0 +1,48 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'edit_custom.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+// ignore_for_file: avoid_private_typedef_functions, non_constant_identifier_names, subtype_of_sealed_class, invalid_use_of_internal_member, unused_element, constant_identifier_names, unnecessary_raw_strings, library_private_types_in_public_api
+
+/// Copied from Dart SDK
+class _SystemHash {
+  _SystemHash._();
+
+  static int combine(int hash, int value) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + value);
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x0007ffff & hash) << 10));
+    return hash ^ (hash >> 6);
+  }
+
+  static int finish(int hash) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x03ffffff & hash) << 3));
+    // ignore: parameter_assignments
+    hash = hash ^ (hash >> 11);
+    return 0x1fffffff & (hash + ((0x00003fff & hash) << 15));
+  }
+}
+
+String _$EditCustomNotifierHash() =>
+    r'0e958e09f416e1f97a707ec33c29ac126221bfe9';
+
+/// See also [EditCustomNotifier].
+final editCustomNotifierProvider = NotifierProvider<EditCustomNotifier, Custom>(
+  EditCustomNotifier.new,
+  name: r'editCustomNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$EditCustomNotifierHash,
+);
+typedef EditCustomNotifierRef = NotifierProviderRef<Custom>;
+
+abstract class _$EditCustomNotifier extends Notifier<Custom> {
+  @override
+  Custom build();
+}
