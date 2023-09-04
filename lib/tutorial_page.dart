906b00e6a81a0eb7d6bfd05d7d4207d6e2df554d
Kai Oishi <bokuchan.kai@gmail.com>
2023-09-04 23:31:13 +0900

    チュートリアルの画像を更新

--- lib/tutorial_page.dart
+++ lib/tutorial_page.dart
@@ -26,10 +26,10 @@ class TutorialPage extends ConsumerWidget {
   }
 
   final pages = [
-    PageModel(color: const Color(0xFF95cedd), imageAssetPath: 'images/tutorial01.png', title: 'カスタムを作成', body: '画面下部から開始できます', doAnimateImage: false),
-    PageModel(color: const Color(0xFF9B90BC), imageAssetPath: 'images/tutorial02.png', title: 'パーツを選択', body: 'キーワード、条件で検索できます', doAnimateImage: true),
-    PageModel(color: const Color(0xFF2B90AC), imageAssetPath: 'images/tutorial03.png', title: 'カスタムの管理', body: '合計金額、パーツ間の互換性を確認できます', doAnimateImage: true),
-    PageModel(color: const Color(0xFFFB90AC), imageAssetPath: 'images/tutorial04.png', title: 'カスタムの確認', body: '保存したカスタムを確認・編集できます', doAnimateImage: true),
+    PageModel(color: const Color(0xFF95cedd), imageAssetPath: 'images/tutorial_v2_01.jpg', title: 'カスタムを作成', body: '画面下部から開始できます', doAnimateImage: false),
+    PageModel(color: const Color(0xFF9B90BC), imageAssetPath: 'images/tutorial_v2_02.jpg', title: 'パーツを選択', body: 'キーワード、条件で検索できます', doAnimateImage: true),
+    PageModel(color: const Color(0xFF2B90AC), imageAssetPath: 'images/tutorial_v2_03.jpg', title: 'カスタムの管理', body: '合計金額、パーツ間の互換性を確認できます', doAnimateImage: true),
+    PageModel(color: const Color(0xFFFB90AC), imageAssetPath: 'images/tutorial_v2_04.jpg', title: 'カスタムの確認', body: '保存したカスタムを確認・編集できます', doAnimateImage: true),
     PageModel.withChild(
         child: const Padding(
             padding: EdgeInsets.only(bottom: 25.0),
