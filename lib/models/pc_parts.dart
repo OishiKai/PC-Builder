e49b6ac4cc29c30cd36b9c03cd5ab9c9e089eb6b
Kai Oishi <bokuchan.kai@gmail.com>
2023-09-11 00:40:07 +0900

    新PcPartsモデル追加

---
+++ lib/models/pc_parts.dart
@@ -0,0 +1,26 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import 'parts_shop.dart';
+
+part 'pc_parts.freezed.dart';
+
+@freezed
+class PcParts with _$PcParts {
+  const PcParts._();
+
+  const factory PcParts({
+    required String id,
+    required String category,
+    required String maker,
+    required bool isNew,
+    required String title,
+    required int? star,
+    required String? evaluation,
+    required String price,
+    required String ranked,
+    required String image,
+    required int fullScaleImageCount,
+    Map<String, String?>? specs,
+    List<PartsShop>? shops,
+  }) = _PcParts;
+}
---
+++ lib/models/pc_parts.freezed.dart
@@ -0,0 +1,408 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'pc_parts.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+mixin _$PcParts {
+  String get id => throw _privateConstructorUsedError;
+  String get category => throw _privateConstructorUsedError;
+  String get maker => throw _privateConstructorUsedError;
+  bool get isNew => throw _privateConstructorUsedError;
+  String get title => throw _privateConstructorUsedError;
+  int? get star => throw _privateConstructorUsedError;
+  String? get evaluation => throw _privateConstructorUsedError;
+  String get price => throw _privateConstructorUsedError;
+  String get ranked => throw _privateConstructorUsedError;
+  String get image => throw _privateConstructorUsedError;
+  int get fullScaleImageCount => throw _privateConstructorUsedError;
+  Map<String, String?>? get specs => throw _privateConstructorUsedError;
+  List<PartsShop>? get shops => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $PcPartsCopyWith<PcParts> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $PcPartsCopyWith<$Res> {
+  factory $PcPartsCopyWith(PcParts value, $Res Function(PcParts) then) =
+      _$PcPartsCopyWithImpl<$Res, PcParts>;
+  @useResult
+  $Res call(
+      {String id,
+      String category,
+      String maker,
+      bool isNew,
+      String title,
+      int? star,
+      String? evaluation,
+      String price,
+      String ranked,
+      String image,
+      int fullScaleImageCount,
+      Map<String, String?>? specs,
+      List<PartsShop>? shops});
+}
+
+/// @nodoc
+class _$PcPartsCopyWithImpl<$Res, $Val extends PcParts>
+    implements $PcPartsCopyWith<$Res> {
+  _$PcPartsCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? category = null,
+    Object? maker = null,
+    Object? isNew = null,
+    Object? title = null,
+    Object? star = freezed,
+    Object? evaluation = freezed,
+    Object? price = null,
+    Object? ranked = null,
+    Object? image = null,
+    Object? fullScaleImageCount = null,
+    Object? specs = freezed,
+    Object? shops = freezed,
+  }) {
+    return _then(_value.copyWith(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      category: null == category
+          ? _value.category
+          : category // ignore: cast_nullable_to_non_nullable
+              as String,
+      maker: null == maker
+          ? _value.maker
+          : maker // ignore: cast_nullable_to_non_nullable
+              as String,
+      isNew: null == isNew
+          ? _value.isNew
+          : isNew // ignore: cast_nullable_to_non_nullable
+              as bool,
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      star: freezed == star
+          ? _value.star
+          : star // ignore: cast_nullable_to_non_nullable
+              as int?,
+      evaluation: freezed == evaluation
+          ? _value.evaluation
+          : evaluation // ignore: cast_nullable_to_non_nullable
+              as String?,
+      price: null == price
+          ? _value.price
+          : price // ignore: cast_nullable_to_non_nullable
+              as String,
+      ranked: null == ranked
+          ? _value.ranked
+          : ranked // ignore: cast_nullable_to_non_nullable
+              as String,
+      image: null == image
+          ? _value.image
+          : image // ignore: cast_nullable_to_non_nullable
+              as String,
+      fullScaleImageCount: null == fullScaleImageCount
+          ? _value.fullScaleImageCount
+          : fullScaleImageCount // ignore: cast_nullable_to_non_nullable
+              as int,
+      specs: freezed == specs
+          ? _value.specs
+          : specs // ignore: cast_nullable_to_non_nullable
+              as Map<String, String?>?,
+      shops: freezed == shops
+          ? _value.shops
+          : shops // ignore: cast_nullable_to_non_nullable
+              as List<PartsShop>?,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$_PcPartsCopyWith<$Res> implements $PcPartsCopyWith<$Res> {
+  factory _$$_PcPartsCopyWith(
+          _$_PcParts value, $Res Function(_$_PcParts) then) =
+      __$$_PcPartsCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String id,
+      String category,
+      String maker,
+      bool isNew,
+      String title,
+      int? star,
+      String? evaluation,
+      String price,
+      String ranked,
+      String image,
+      int fullScaleImageCount,
+      Map<String, String?>? specs,
+      List<PartsShop>? shops});
+}
+
+/// @nodoc
+class __$$_PcPartsCopyWithImpl<$Res>
+    extends _$PcPartsCopyWithImpl<$Res, _$_PcParts>
+    implements _$$_PcPartsCopyWith<$Res> {
+  __$$_PcPartsCopyWithImpl(_$_PcParts _value, $Res Function(_$_PcParts) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? category = null,
+    Object? maker = null,
+    Object? isNew = null,
+    Object? title = null,
+    Object? star = freezed,
+    Object? evaluation = freezed,
+    Object? price = null,
+    Object? ranked = null,
+    Object? image = null,
+    Object? fullScaleImageCount = null,
+    Object? specs = freezed,
+    Object? shops = freezed,
+  }) {
+    return _then(_$_PcParts(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      category: null == category
+          ? _value.category
+          : category // ignore: cast_nullable_to_non_nullable
+              as String,
+      maker: null == maker
+          ? _value.maker
+          : maker // ignore: cast_nullable_to_non_nullable
+              as String,
+      isNew: null == isNew
+          ? _value.isNew
+          : isNew // ignore: cast_nullable_to_non_nullable
+              as bool,
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      star: freezed == star
+          ? _value.star
+          : star // ignore: cast_nullable_to_non_nullable
+              as int?,
+      evaluation: freezed == evaluation
+          ? _value.evaluation
+          : evaluation // ignore: cast_nullable_to_non_nullable
+              as String?,
+      price: null == price
+          ? _value.price
+          : price // ignore: cast_nullable_to_non_nullable
+              as String,
+      ranked: null == ranked
+          ? _value.ranked
+          : ranked // ignore: cast_nullable_to_non_nullable
+              as String,
+      image: null == image
+          ? _value.image
+          : image // ignore: cast_nullable_to_non_nullable
+              as String,
+      fullScaleImageCount: null == fullScaleImageCount
+          ? _value.fullScaleImageCount
+          : fullScaleImageCount // ignore: cast_nullable_to_non_nullable
+              as int,
+      specs: freezed == specs
+          ? _value._specs
+          : specs // ignore: cast_nullable_to_non_nullable
+              as Map<String, String?>?,
+      shops: freezed == shops
+          ? _value._shops
+          : shops // ignore: cast_nullable_to_non_nullable
+              as List<PartsShop>?,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_PcParts extends _PcParts {
+  const _$_PcParts(
+      {required this.id,
+      required this.category,
+      required this.maker,
+      required this.isNew,
+      required this.title,
+      required this.star,
+      required this.evaluation,
+      required this.price,
+      required this.ranked,
+      required this.image,
+      required this.fullScaleImageCount,
+      final Map<String, String?>? specs,
+      final List<PartsShop>? shops})
+      : _specs = specs,
+        _shops = shops,
+        super._();
+
+  @override
+  final String id;
+  @override
+  final String category;
+  @override
+  final String maker;
+  @override
+  final bool isNew;
+  @override
+  final String title;
+  @override
+  final int? star;
+  @override
+  final String? evaluation;
+  @override
+  final String price;
+  @override
+  final String ranked;
+  @override
+  final String image;
+  @override
+  final int fullScaleImageCount;
+  final Map<String, String?>? _specs;
+  @override
+  Map<String, String?>? get specs {
+    final value = _specs;
+    if (value == null) return null;
+    if (_specs is EqualUnmodifiableMapView) return _specs;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableMapView(value);
+  }
+
+  final List<PartsShop>? _shops;
+  @override
+  List<PartsShop>? get shops {
+    final value = _shops;
+    if (value == null) return null;
+    if (_shops is EqualUnmodifiableListView) return _shops;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(value);
+  }
+
+  @override
+  String toString() {
+    return 'PcParts(id: $id, category: $category, maker: $maker, isNew: $isNew, title: $title, star: $star, evaluation: $evaluation, price: $price, ranked: $ranked, image: $image, fullScaleImageCount: $fullScaleImageCount, specs: $specs, shops: $shops)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_PcParts &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.category, category) ||
+                other.category == category) &&
+            (identical(other.maker, maker) || other.maker == maker) &&
+            (identical(other.isNew, isNew) || other.isNew == isNew) &&
+            (identical(other.title, title) || other.title == title) &&
+            (identical(other.star, star) || other.star == star) &&
+            (identical(other.evaluation, evaluation) ||
+                other.evaluation == evaluation) &&
+            (identical(other.price, price) || other.price == price) &&
+            (identical(other.ranked, ranked) || other.ranked == ranked) &&
+            (identical(other.image, image) || other.image == image) &&
+            (identical(other.fullScaleImageCount, fullScaleImageCount) ||
+                other.fullScaleImageCount == fullScaleImageCount) &&
+            const DeepCollectionEquality().equals(other._specs, _specs) &&
+            const DeepCollectionEquality().equals(other._shops, _shops));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      id,
+      category,
+      maker,
+      isNew,
+      title,
+      star,
+      evaluation,
+      price,
+      ranked,
+      image,
+      fullScaleImageCount,
+      const DeepCollectionEquality().hash(_specs),
+      const DeepCollectionEquality().hash(_shops));
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$_PcPartsCopyWith<_$_PcParts> get copyWith =>
+      __$$_PcPartsCopyWithImpl<_$_PcParts>(this, _$identity);
+}
+
+abstract class _PcParts extends PcParts {
+  const factory _PcParts(
+      {required final String id,
+      required final String category,
+      required final String maker,
+      required final bool isNew,
+      required final String title,
+      required final int? star,
+      required final String? evaluation,
+      required final String price,
+      required final String ranked,
+      required final String image,
+      required final int fullScaleImageCount,
+      final Map<String, String?>? specs,
+      final List<PartsShop>? shops}) = _$_PcParts;
+  const _PcParts._() : super._();
+
+  @override
+  String get id;
+  @override
+  String get category;
+  @override
+  String get maker;
+  @override
+  bool get isNew;
+  @override
+  String get title;
+  @override
+  int? get star;
+  @override
+  String? get evaluation;
+  @override
+  String get price;
+  @override
+  String get ranked;
+  @override
+  String get image;
+  @override
+  int get fullScaleImageCount;
+  @override
+  Map<String, String?>? get specs;
+  @override
+  List<PartsShop>? get shops;
+  @override
+  @JsonKey(ignore: true)
+  _$$_PcPartsCopyWith<_$_PcParts> get copyWith =>
+      throw _privateConstructorUsedError;
+}
