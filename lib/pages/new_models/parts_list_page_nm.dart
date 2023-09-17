2f5e31df6a409a845d17f0997f795b7f7e1c7b33
Kai Oishi <bokuchan.kai@gmail.com>
2023-09-18 00:41:39 +0900

    mainに戻るよ

--- lib/pages/new_models/parts_list_page_nm.dart
+++ lib/pages/new_models/parts_list_page_nm.dart
@@ -1,4 +1,9 @@
+import 'package:custom_pc/nm_widgets/search_result_parts_list_widget.dart';
 import 'package:flutter/material.dart';
+
+import '../../widgets/parts_list_page/parameters_select_drawer.dart';
+import '../../widgets/parts_list_page/parts_search_app_bar.dart';
+import '../../widgets/parts_list_page/searching_parameter_widget.dart';
 
 class PartsListPageNM extends StatelessWidget {
   const PartsListPageNM({Key? key}) : super(key: key);
@@ -6,7 +11,14 @@ class PartsListPageNM extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(child: Text('PartsListPageNM')),
+      appBar: PartsSearchAppBar(),
+      endDrawer: ParametersSelectDrawer(),
+      body: Column(
+        children: [
+          SearchingParameterWidget(),
+          SearchResultPartsListWidgetNM(),
+        ],
+      ),
     );
   }
 }
